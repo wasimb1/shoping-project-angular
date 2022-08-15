129f5c03317fbdf25450a269b746d0c4feeb97e0
WaseemOB <wasimbaba.wb1@gmail.com>
2022-08-15 18:17:17 +0530

    added section wise access to pages in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shoping-project';
+  selPage: string =  'recipe-list';
+  onSelectedPage(page:string) {
+    this.selPage = page;
+  }
 }
